0294b6661cef7826785ff9351ede950e3f0ee26d
DanaKeimel <88860131+DanaKeimel@users.noreply.github.com>
2021-11-24 21:45:47 -0500

    Initial Copy of Final Project Code

---
+++ src/script.js
@@ -0,0 +1,67 @@
+let time = new Date();
+
+//Day and time
+let hours = time.getHours();
+
+let minutes = time.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[time.getDay()];
+let h2 = document.querySelector("h2");
+h2.innerHTML = `${day}, ${hours}:${minutes}`;
+
+// Search engine
+
+function displayWeather(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperatureNumber").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  response.data.weather[0].description.toUpperCase();
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = response.data.wind.speed;
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+}
+function searchCity(city) {
+  let apiKey = "833e82c86865467ec6b58ba7805f7c0a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+function buttonSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-text-input").value;
+  searchCity(city);
+}
+function searchLocation(position) {
+  let apiKey = "833e82c86865467ec6b58ba7805f7c0a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", buttonSubmit);
+
+let currentLocationButton = document.querySelector("#current-location-btn");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Toronto");
